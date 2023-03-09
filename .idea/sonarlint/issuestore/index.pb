4859f17ee53db9ac0e14e92c8f52ba52ab829094
Fábio Souza <fsdssouza@gmail.com>
2023-03-09 14:22:31 -0300

    criado ContratoTrabalho e Funcionario

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+PagamentoSalario.iml,3\4\34b30f7559e4e466f5bc5cb0ac6ce72cd44e85aa
---
+++ src/ContratoTrabalho.java
@@ -0,0 +1,6 @@
+public class ContratoTrabalho {
+
+    Funcionario funcionario;
+    double valorHoraTrabalhoNormal;
+    double valorHoraTrabalhoExtra;
+}
