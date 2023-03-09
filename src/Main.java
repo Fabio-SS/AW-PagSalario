809eb4288b7e05edbdbf4999a1184086dd55e9a1
Fábio Souza <fsdssouza@gmail.com>
2023-03-09 14:12:22 -0300

    Commit Init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FolhaPagamento folhaPagamento = new FolhaPagamento();
+
+        System.out.println(folhaPagamento.calcularSalario(5,2,2,5));
+    }
+}
